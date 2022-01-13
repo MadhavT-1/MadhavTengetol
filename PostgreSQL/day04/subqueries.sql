8d115a5f1861bc3639ae282a9a5c566480e5583a
MadhavT-1 <62096719+MadhavT-1@users.noreply.github.com>
2022-01-13 14:26:47 +0530

    day04

--- PostgreSQL/day04/subqueries.sql
+++ PostgreSQL/day04/subqueries.sql
@@ -35,5 +35,52 @@
                   JOIN orders ON order_details.orderid=orders.orderid
              	WHERE suppliers.supplierid=products.supplierid AND
                   orderdate BETWEEN '1996-12-01' AND '1996-12-31' );
+				  
+				  
+	=========================	ANY and ALL 	=======================
+
+6.	SELECT companyname
+	FROM customers
+	WHERE  customerid = ANY (SELECT customerid FROM orders
+                         JOIN order_details ON orders.orderid=order_details.orderid
+                         WHERE quantity > 50);
+
+7.	SELECT companyname
+	FROM suppliers
+	WHERE  supplierid = ANY (SELECT products.supplierid FROM order_details
+                        JOIN products ON products.productid=order_details.productid
+                        WHERE quantity = 1);
+
+8.	SELECT DISTINCT productname
+	FROM products
+	JOIN order_details ON products.productid=order_details.productid
+	WHERE  order_details.unitprice*quantity > ALL
+	(SELECT AVG(order_details.unitprice*quantity)
+             FROM order_details
+             GROUP BY productid);
+
+ 9.	SELECT DISTINCT companyname
+	FROM customers
+	JOIN orders ON orders.customerid=customers.customerid
+	JOIN order_details ON orders.orderid=order_details.orderid
+	WHERE  order_details.unitprice*quantity > ALL
+ 	(SELECT AVG(order_details.unitprice*quantity)
+              FROM order_details
+             JOIN orders ON orders.orderid=order_details.orderid
+             GROUP BY orders.customerid);
+	
                                
+	====================== 	IN using subquery 	=====================
+10.	SELECT companyname
+	FROM customers
+	WHERE country IN (SELECT DISTINCT country 
+	FROM suppliers);
+
+11.	SELECT companyname
+	FROM suppliers
+	WHERE city IN (SELECT DISTINCT city
+	FROM customers);
+
+                  
+	
 	
